e628450a297534536e2e3c5cea03d1a4b30a9e2d
hiding <hiding2222@gmail.com>
2020-08-19 17:10:42 +0700

    add todo list

--- src/App.js
+++ src/App.js
@@ -7,17 +7,11 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <ul>
+          <li className='todos' >Go to market</li>
+          <li className='todos' >Buy food</li>
+          <li className='todos' >Make dinner</li>
+        </ul>
       </header>
     </div>
   );
